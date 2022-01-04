e7aba8ab8cbc3c59f25c7026f17fd3244f777fd7
hiagoLF <hiagolof@gmail.com>
2022-01-04 13:46:06 -0300

    Extra ssl

--- ormconfig.js
+++ ormconfig.js
@@ -1,26 +1,50 @@
-const entities = process.env.AMBIENT === "development" ? "src/typeorm/entities/*.ts" : "dist/typeorm/entities/*.js"
-const migrations = process.env.AMBIENT === "development" ? "src/typeorm/migrations/*.ts" : "dist/typeorm/migrations/*.js"
-const userName = process.env.AMBIENT === 'production' ? process.env.PRODUCTION_POSTGRES_USER_NAME : process.env.DEVELOPMENT_POSTGRES_USER_NAME
-const password = process.env.AMBIENT === 'production' ? process.env.PRODUCTION_POSTGRES_PASSWORD : process.env.DEVELOPMENT_POSTGRES_PASSWORD
-const dataBase =  process.env.AMBIENT === 'production' ? process.env.PRODUCTION_POSTGRES_DATABASE : process.env.DEVELOPMENT_POSTGRES_DATABASE
-const host =  process.env.AMBIENT === 'production' ? process.env.PRODUCTION_POSTGRES_HOST : process.env.DEVELOPMENT_POSTGRES_HOST
+const entities =
+  process.env.AMBIENT === "development"
+    ? "src/typeorm/entities/*.ts"
+    : "dist/typeorm/entities/*.js";
+const migrations =
+  process.env.AMBIENT === "development"
+    ? "src/typeorm/migrations/*.ts"
+    : "dist/typeorm/migrations/*.js";
+const userName =
+  process.env.AMBIENT === "production"
+    ? process.env.PRODUCTION_POSTGRES_USER_NAME
+    : process.env.DEVELOPMENT_POSTGRES_USER_NAME;
+const password =
+  process.env.AMBIENT === "production"
+    ? process.env.PRODUCTION_POSTGRES_PASSWORD
+    : process.env.DEVELOPMENT_POSTGRES_PASSWORD;
+const dataBase =
+  process.env.AMBIENT === "production"
+    ? process.env.PRODUCTION_POSTGRES_DATABASE
+    : process.env.DEVELOPMENT_POSTGRES_DATABASE;
+const host =
+  process.env.AMBIENT === "production"
+    ? process.env.PRODUCTION_POSTGRES_HOST
+    : process.env.DEVELOPMENT_POSTGRES_HOST;
+const url = process.env.DATABASE_URL;
 
-console.log('userName', userName)
-console.log('password', password)
-console.log('dataBase', dataBase)
-console.log('host', host)
+console.log("userName", userName);
+console.log("password", password);
+console.log("dataBase", dataBase);
+console.log("host", host);
+console.log("entities >>> ", entities);
+console.log("databaseurl >>> ", url);
 
 module.exports = {
-  "type": "postgres",
-  "url": process.env.DATABASE_URL,
-  // "host": host,
-  // "port": 5432,
-  // "username": userName,
-  // "password":password,
-  // "database": dataBase,
-  "synchronize": true,
-  "entities": [entities],
-  "migrations": [migrations],
-  "seeds": ["src/typeorm/seeds/*.ts"],
-  "factories": ["src/typeorm/factories/*.ts"]
-}
+  type: "postgres",
+  // "url": url,
+  host: host,
+  port: 5432,
+  username: userName,
+  password: password,
+  database: dataBase,
+  extra: {
+    ssl: true,
+  },
+  synchronize: true,
+  entities: [entities],
+  migrations: [migrations],
+  seeds: ["src/typeorm/seeds/*.ts"],
+  factories: ["src/typeorm/factories/*.ts"],
+};
